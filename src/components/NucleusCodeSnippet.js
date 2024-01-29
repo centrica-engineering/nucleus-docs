c85e345b2d8bd7f3ac3bf22aa6bed9ca04b141fe
MekalaNagarajan_Centrica <mekala.nagarajan@centrica.com>
2024-01-29 22:28:11 +0000

    feat: expressive code snippet

--- src/components/NucleusCodeSnippet.js
+++ src/components/NucleusCodeSnippet.js
@@ -1,6 +1,7 @@
 import { LitElement, html, css } from 'lit';
 import { unsafeHTML } from 'lit/directives/unsafe-html.js';
-import { getHighlighter } from 'shikiji';
+import { createRenderer } from 'remark-expressive-code';
+import { toHtml } from 'hast-util-to-html';
 import { Task } from '@lit/task';
 
 export class NucleusCodeSnippet extends LitElement {
@@ -15,8 +16,35 @@ export class NucleusCodeSnippet extends LitElement {
     super();
     this._formattedSrc = new Task(this, {
       task: async ([src], {signal} ) => {
-        const highlighter = await getHighlighter({ themes: ['github-dark', 'github-light'] });
-        const htmlContent = highlighter.codeToHtml(src, {theme: 'github-dark'});
+        const {ec, themeStyles, baseStyles, jsModules} = await createRenderer({
+          themes:['github-dark', 'github-light'],
+          useDarkModeMediaQuery: true
+        });
+        const { renderedGroupAst, styles } = await ec.render({
+          code: src,
+          language: 'html',
+          meta: ''
+        });
+
+        console.log(themeStyles);
+        let htmlContent = toHtml(renderedGroupAst);
+        const frameSelector = 'figure class="frame';
+        let index = htmlContent.indexOf(frameSelector);
+        if(index > 0) {
+          htmlContent = htmlContent.split(frameSelector).join(`${frameSelector} not-content`);
+        }
+        //map data-theme to the code block theme
+        const themedStyles = themeStyles?.replaceAll(':root', ':host').replaceAll('github-dark', 'light').replaceAll('github-light', 'dark');
+        // Collect styles and add them before the HTML content
+        const prefixToLookup = 'class="expressive-code">';
+        const htmlContentParts = htmlContent.split(prefixToLookup);
+        htmlContent = `${htmlContentParts[0]}${prefixToLookup}
+          ${(baseStyles || themedStyles) ? '<style>' : ''}
+          ${baseStyles ?? ''}
+          ${themedStyles ?? ''}
+          ${(baseStyles || themedStyles) ? '</style>' : ''}
+          <script>${jsModules}</script>
+          ${htmlContentParts[1]}`;
         return html`${unsafeHTML(htmlContent)}`;
       },
       args: () => [this.src]
