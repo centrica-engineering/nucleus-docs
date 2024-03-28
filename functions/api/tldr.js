091ba2526599ea00e888c71c2ad8325bb5fa82e4
jholt1 <jonathan.holt@centrica.com>
2024-03-28 15:25:37 +0000

    chore: move to api

--- functions/api/tldr.js
+++ functions/api/tldr.js
@@ -70,9 +70,8 @@ export default {
     }
 
     function removeEmojis(text) {
-      // Regular expression to match emojis
       const emojiRegex = /[\u{1F600}-\u{1F64F}|\u{1F300}-\u{1F5FF}|\u{1F680}-\u{1F6FF}|\u{2600}-\u{26FF}|\u{2700}-\u{27BF}]/ug;
-      // Remove emojis from the text
+
       return text.replace(emojiRegex, '');
     }
   }
