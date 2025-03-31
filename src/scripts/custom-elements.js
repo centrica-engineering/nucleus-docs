ecf70947aec2c2ad517a8c501d4fd5ba1dcdb644
MekalaNagarajan_Centrica <mekala.nagarajan@centrica.com>
2025-03-31 15:14:34 +0100

    build: snowflake components custom elements

--- src/scripts/custom-elements.js
+++ src/scripts/custom-elements.js
@@ -1,4 +1,6 @@
 import ceJsDoc from "@connectedhomes/nucleus/ce-doc.json";
 import customElements from "@connectedhomes/nucleus/custom-elements.json";
+import sfCEJsDoc from "@connectedhomes/nucleus-snowflakes/ce-doc.json";
+import sfCustomElements from "@connectedhomes/nucleus-snowflakes/custom-elements.json";
 
-export { customElements, ceJsDoc };
+export { customElements, ceJsDoc, sfCustomElements, sfCEJsDoc };
