fa34d150e7493cbc530b5a48514f6d2e8a258313
Drew Jones <drew.jones@centrica.com>
2025-08-04 16:07:55 +0100

    feat: adding data-theme to Configurator from localStorage

--- astro.config.mjs
+++ astro.config.mjs
@@ -135,7 +135,6 @@ export default defineConfig({
         PageTitle: "./src/components/starlight/PageTitle.astro",
         PageSidebar: "./src/components/starlight/PageSidebar.astro",
         TableOfContents: "./src/components/starlight/TableOfContents.astro",
-        // ThemeSelect: './src/components/starlight/ThemeSelect.astro',
         TwoColumnContent: "./src/components/starlight/TwoColumnContent.astro",
         Sidebar: "./src/components/starlight/Sidebar.astro"
       },
